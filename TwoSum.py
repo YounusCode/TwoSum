83772c28f005c16fd24a1c26dcd26e488d494e5e
Younus Ali <younus_ali45@icloud.com>
2023-08-14 12:42:39 -0500

    Add files via upload

---
+++ TwoSum.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :type: List[int]
+        """
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+        return []
+
+if __name__ == '__main__':
+    s = Solution()
+    print (s.twoSum([2, 7, 11, 15], 18))
